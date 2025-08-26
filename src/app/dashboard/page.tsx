d65b79478496f2ae59a7d7f9f040c4a8440c290f
Edgar Barragan Garcia <edgarbarragangarcia@gmail.com>
2025-08-26 22:52:27 +0000

    <div data-state="inactive" data-orientation="horizontal" role="tabpanel"

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,8 @@
 
 import { AppLayout } from "@/components/layout/app-layout";
 import { DashboardPage } from "@/components/pages/dashboard-page";
-import { GeneralGanttChart } from "@/components/project/general-gantt-chart";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useProjects } from "@/hooks/use-projects";
 import { useTasks } from "@/hooks/use-tasks";
-import { BarChartHorizontal, LayoutDashboard } from "lucide-react";
 
 export default function DashboardRoute() {
   const { tasks } = useTasks();
@@ -14,20 +11,7 @@ export default function DashboardRoute() {
   return (
     <main className="flex flex-col h-full">
       <AppLayout>
-         <Tabs defaultValue="dashboard" className="flex-1 flex flex-col overflow-hidden">
-          <div className='px-4 py-2 border-b'>
-            <TabsList>
-              <TabsTrigger value="dashboard"><LayoutDashboard className='size-4 mr-2'/>Dashboard</TabsTrigger>
-              <TabsTrigger value="gantt"><BarChartHorizontal className='size-4 mr-2'/>Gantt General</TabsTrigger>
-            </TabsList>
-          </div>
-          <TabsContent value="dashboard" className="flex-1 flex flex-col overflow-y-auto mt-0">
-            <DashboardPage />
-          </TabsContent>
-          <TabsContent value="gantt" className="flex-1 flex flex-col overflow-y-hidden mt-0">
-             <GeneralGanttChart tasks={tasks} projects={projects} />
-          </TabsContent>
-        </Tabs>
+        <DashboardPage />
       </AppLayout>
     </main>
   );
